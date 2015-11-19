92227606920fde88936566965ae1bb2c1dfa050f
KimmyLeo <kenpusney@gmail.com>
2015-11-19 12:55:26 +0800

    reformat

--- src/test/java/ns/tw/guess/CompareNumberTest.java
+++ src/test/java/ns/tw/guess/CompareNumberTest.java
@@ -1,14 +1,22 @@
 package ns.tw.guess;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
 public class CompareNumberTest {
+
+    private CompareNumber compareNumber;
+
+    @Before
+    public void setUp() throws Exception {
+        compareNumber = new CompareNumber();
+    }
+
     @Test
     public void should_show_tip_4A0B_when_compare_1234_and_1234() throws Exception {
-        CompareNumber compareNumber = new CompareNumber();
         String generated = "1234";
         String guessed = "1234";
 
@@ -19,7 +27,6 @@ public class CompareNumberTest {
 
     @Test
     public void should_show_tip_0A0B_when_compare_1234_and_5678() throws Exception {
-        CompareNumber compareNumber = new CompareNumber();
         String generated = "1234";
         String guessed = "5678";
 
@@ -30,7 +37,6 @@ public class CompareNumberTest {
 
     @Test
     public void should_show_tip_0A4B_when_compare_1234_and_4321() throws Exception {
-        CompareNumber compareNumber = new CompareNumber();
         String generated = "1234";
         String guessed = "4321";
 
@@ -41,7 +47,6 @@ public class CompareNumberTest {
 
     @Test
     public void should_show_tip_2A2B_when_compare_1234_and_1324() throws Exception {
-        CompareNumber compareNumber = new CompareNumber();
         String generated = "1234";
         String guessed = "1324";
 
