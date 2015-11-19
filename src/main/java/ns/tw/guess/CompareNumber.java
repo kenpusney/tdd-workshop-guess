ebd8d6eb0d36cfd37f1fc9fbd5674e5eebbdb78c
KimmyLeo <kenpusney@gmail.com>
2015-11-19 12:30:14 +0800

    step1

---
+++ src/main/java/ns/tw/guess/CompareNumber.java
@@ -0,0 +1,18 @@
+package ns.tw.guess;
+
+public class CompareNumber {
+
+    public String compare(String generated, String guessed) {
+        int aCount = 0;
+        int containedCount = 0;
+        for (int index = 0; index < generated.length(); index ++) {
+            if(generated.charAt(index) == guessed.charAt(index)) {
+                aCount++;
+            }
+            if(guessed.indexOf(generated.charAt(index)) != -1) {
+                containedCount++;
+            }
+        }
+        return String.format("%dA%dB", aCount, containedCount - aCount);
+    }
+}
---
+++ src/test/java/ns/tw/guess/CompareNumberTest.java
@@ -0,0 +1,52 @@
+package ns.tw.guess;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class CompareNumberTest {
+    @Test
+    public void should_show_tip_4A0B_when_compare_1234_and_1234() throws Exception {
+        CompareNumber compareNumber = new CompareNumber();
+        String generated = "1234";
+        String guessed = "1234";
+
+        String tip = compareNumber.compare(generated, guessed);
+
+        assertThat(tip, is("4A0B"));
+    }
+
+    @Test
+    public void should_show_tip_0A0B_when_compare_1234_and_5678() throws Exception {
+        CompareNumber compareNumber = new CompareNumber();
+        String generated = "1234";
+        String guessed = "5678";
+
+        String tip = compareNumber.compare(generated, guessed);
+
+        assertThat(tip, is("0A0B"));
+    }
+
+    @Test
+    public void should_show_tip_0A4B_when_compare_1234_and_4321() throws Exception {
+        CompareNumber compareNumber = new CompareNumber();
+        String generated = "1234";
+        String guessed = "4321";
+
+        String tip = compareNumber.compare(generated, guessed);
+
+        assertThat(tip, is("0A4B"));
+    }
+
+    @Test
+    public void should_show_tip_2A2B_when_compare_1234_and_1324() throws Exception {
+        CompareNumber compareNumber = new CompareNumber();
+        String generated = "1234";
+        String guessed = "1324";
+
+        String tip = compareNumber.compare(generated, guessed);
+
+        assertThat(tip, is("2A2B"));
+    }
+}
